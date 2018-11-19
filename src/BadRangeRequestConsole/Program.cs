1074c26c13ae480011f958cd8ba58a1b51d93ef4
MatthiasLaroche <larochematthias@gmail.com>
2018-11-19 13:36:59 +0100

    Initial upload
    Simple Web API serving a file with a console client reproducing the bug.

---
+++ src/BadRangeRequestConsole/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace BadRangeRequestConsole
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            const string url = @"http://localhost:5000/api/file";
+            using (var client = new HttpClient())
+            {
+                // We download the file a first time to check everything works fine and get the file length
+                var fileBytes = await client.GetByteArrayAsync(url);
+
+                // We request a range after the end of the file
+                client.DefaultRequestHeaders.Range = new RangeHeaderValue(fileBytes.Length, null);
+                var response = await client.GetAsync(url);
+
+                // We get a "500 Internal Server Error" instead of a 416
+                Console.WriteLine($"{(int) response.StatusCode} - {response.ReasonPhrase}");
+            }
+        }
+    }
+}
---
+++ src/FileApi/FileController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FileApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        [HttpGet]
+        public FileResult Get()
+        {
+            return File(@"testFile.json", "application/json", true);
+        }
+    }
+}
